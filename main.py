d35d637003b48c6cc425a6d812dcfc579b78ddcc
redCode <nicolasimh@gmail.com>
2023-12-01 16:18:40 -0300

    archivo de dependencias para despliegue

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # una vez instalado Python
 # pip install fastapi
 # pip install uvicorn
+# pip install --upgrade pip
+# python -m pip freeze > requirements.txt
+# pip install --upgrade pip && pip install -r requirements.txt
+# uvicorn main:app --host 0.0.0.0 --port 10000
+#
 import uvicorn
 from fastapi import FastAPI
 from routers import usuarios
